fd622d87a441f382ecfca9108646760e8f5e2c96
DinoRozing <dino.rozing555@gmail.com>
2024-06-03 01:38:36 +0200

    Commited update method

---
+++ update.sql
@@ -0,0 +1,6 @@
+UPDATE "restaurant"
+SET "name" = 'Lipov Hlad',
+    "address" = 'Trg bana Josipa Jelačića 2, Osijek',
+    "number" = '0965289880',
+    "customerId" = 2
+WHERE "name" = 'Restaurant Novi Salon';
